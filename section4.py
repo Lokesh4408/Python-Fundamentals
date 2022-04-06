7c5db4953472909b858d2504e1217916242a8557
Venkata Lokesh Reddy Munagala <venkata-lokesh-reddy.munagala@tu-ilmenau.de>
2022-04-06 23:53:20 +0200

    ifelse

--- section4.py
+++ section4.py
@@ -1,1 +1,13 @@
 # Conditional logic
+is_old = False
+is_lincensed = False
+
+if is_old:
+    print('You are old enough to drive.')
+    
+elif is_lincensed:
+    print('You have a valid license! you can go now')
+    
+else:
+    print('You don\'t have a valid license, you faked it. So you are under arrest.')
+    
