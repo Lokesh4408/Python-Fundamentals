8045acf8a9e54142615bd676e2a276c4462bf0c9
Venkata Lokesh Reddy Munagala <venkata-lokesh-reddy.munagala@tu-ilmenau.de>
2022-04-22 21:40:14 +0200

    cat exercise

--- section6.py
+++ section6.py
@@ -1,13 +1,21 @@
-#OOP
+# OOP
 class PlayerCharacter:
-    def __init__(self, name, age):
-        self.name = name 
-        self.age = age
-        
+    # Class Object Attribute
+    membership = True
+
+    def __init__(self, name, age):  # this is a constructor function
+        if (self.membership):
+            self.name = name  # attributes
+            self.age = age
+
     def run(self):
         print('run')
         return 'done'
-        
+
+    def shout(self):
+        print(f'my name is {self.name}')
+
+
 player1 = PlayerCharacter('Lokesh', 25)
 player2 = PlayerCharacter('Erik', 24)
 
@@ -16,5 +24,27 @@ print(player2.name)
 print(player1.age)
 print(player2.age)
 print(player1.run())
-print(player1)
-print(player2)
+print(player1.membership)
+print(player2.membership)
+print(player1.shout())
+print(player2.shout())
+
+# Cat exercise
+
+
+class Cat:
+    species = 'mammal'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# 1 Instantiate the Cat object with 3 cats
+cat1 = Cat('Billy', 1)
+cat2 = Cat('Bobby', 2)
+cat3 = Cat('Brown', 3)
+
+# 2 Create a function that finds the oldest cat
+
+print(cat1.age)
