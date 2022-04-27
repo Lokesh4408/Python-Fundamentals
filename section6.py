73add0c82d445ba23463b09d684b2d962163a379
Venkata Lokesh Reddy Munagala <venkata-lokesh-reddy.munagala@tu-ilmenau.de>
2022-04-27 14:04:40 +0200

    adding solution for cat exercise using oop

--- section6.py
+++ section6.py
@@ -1,4 +1,7 @@
 # OOP
+import numpy as np
+
+
 class PlayerCharacter:
     # Class Object Attribute
     membership = True
@@ -14,8 +17,16 @@ class PlayerCharacter:
 
     def shout(self):
         print(f'my name is {self.name}')
+        
+    @classmethod
+    def addings_things(cls, num1, num2):
+        return cls('Teddy', num1 + num2)
+    
+    @staticmethod
+    def adding_things2(num1, num2):
+        return num1 + num2
 
-
+print(PlayerCharacter.addings_things(21,3)) # classmethods can be used before instanciating the classes.
 player1 = PlayerCharacter('Lokesh', 25)
 player2 = PlayerCharacter('Erik', 24)
 
@@ -42,9 +53,35 @@ class Cat:
 
 # 1 Instantiate the Cat object with 3 cats
 cat1 = Cat('Billy', 1)
-cat2 = Cat('Bobby', 2)
-cat3 = Cat('Brown', 3)
+cat2 = Cat('Bobby', 8)
+cat3 = Cat('Brown', 6)
 
 # 2 Create a function that finds the oldest cat
 
+
+def oldest(cat1, cat2, cat3):
+    #count = 0
+    oldest = []
+    oldest.append(cat1.age)
+    oldest.append(cat2.age)
+    oldest.append(cat3.age)
+    return np.max(oldest)
+
+
 print(cat1.age)
+
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+Eldercat = oldest(cat1, cat2, cat3)
+print(f'The eldest cat is {Eldercat} years old.')
+
+# Andrei's solution
+peanut = Cat('Peanut', 3)
+garfield = Cat('Garfield', 5)
+snickers = Cat('Snickers', 1)
+
+def get_oldest_cat(*args):
+    return max(args)
+
+print(f'The oldest cat is {get_oldest_cat(peanut.age, garfield.age, snickers.age)} years old.')
+
+ 
