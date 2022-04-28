a458c2843a63b9172596892dd55587dfdbdcc4b8
Venkata Lokesh Reddy Munagala <82271199+Lokesh4408@users.noreply.github.com>
2022-04-28 23:58:31 +0200

    Encapsulation
    Differentiating encapsulation functionality from using a dictionary

---
+++ section6_1.py
@@ -0,0 +1,14 @@
+# 4 pillars of OOP: Encapsulation, Abstraction, Inheritance and Polymorphism
+# Encapsulation
+class PlayerCharacter:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+player1 = PlayerCharacter('lokesh', 25)
+print(player1.name)
+print(player1.age)
+
+player2 = {'name': 'andrei', 'age': 25}
+print(player2['name'])
+print(player2['age'])
