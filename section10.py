f3ccaed1c5e36e7d9da662b88baa423a8e737b84
Venkata Lokesh Reddy Munagala <venkata-lokesh-reddy.munagala@tu-ilmenau.de>
2022-11-06 21:15:08 +0100

    additions

--- section10.py
+++ section10.py
@@ -1,1 +1,11 @@
 # Generators
+
+# def sum(num1, num2):
+#     try:
+#         return num1/num2
+#     except (TypeError, ZeroDivisionError) as err:
+#         #print(f'please enter numbers {err}')
+#         print(err)
+#         print('oops')
+
+# print(sum(1,0))
